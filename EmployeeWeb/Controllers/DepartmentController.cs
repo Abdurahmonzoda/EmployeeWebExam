00c43e707cd554be6600666dbae2b34c84879954
Abdurahmonzoda <86366309+Abdurahmonzoda@users.noreply.github.com>
2022-09-24 22:10:57 +0500

    Exam was easy

---
+++ EmployeeWeb/Controllers/DepartmentController.cs
@@ -0,0 +1,40 @@
+﻿using Domail.Response;
+using Domain.Dto;
+using Domain.Emtities;
+using Microsoft.AspNetCore.Mvc;
+using Services.Services;
+
+namespace EmployeeWeb.Controllers
+{
+    public class DepartmentController
+    {
+        private DepartmentService _departmentService;
+        public DepartmentController(DepartmentService departmentService)
+        {
+            _departmentService = departmentService;
+        }
+        [HttpGet("GetDepartments")]
+        public async Task<Response<List<DepartmentDto>>> GetDepartments()
+        {
+            return await _departmentService.GetDepartments();
+        }
+
+        [HttpGet("GetDepartmentById")]
+        public async Task<Response<List<DepartmentDto>>> GetDepartmentById(int id)
+        {
+            return await _departmentService.GetDepartmentById(id);
+        }
+
+        [HttpPost("AddDepartment")]
+        public async Task<Response<Department>> AddDepartment(Department department)
+        {
+            return await _departmentService.AddDepartment(department);
+        }
+
+        [HttpPut("UpdateDepartment")]
+        public async Task<Response<Department>> UpdateDepartment(Department department)
+        {
+            return await _departmentService.UpdateDepartment(department);
+        }
+    }
+}
---
+++ Services/Services/DepartmentService.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using Dapper;
+using Domail.Response;
+using Domain.Dto;
+using Domain.Emtities;
+using Services.DateContext; 
+namespace Services.Services
+{
+    public class DepartmentService
+    {
+        private Context _context;
+        public DepartmentService(Context context)
+        {
+              _context = context;
+        }
+
+        public async Task<Response<List<DepartmentDto>>> GetDepartments()
+        {
+            var connection = _context.CreateConnection();
+            string sql = "select d.id ,  d.name , dm.EmployeeId as ManagerId ,concat(emp.firstname , ' ' , emp.lastname) as ManagerFullName FROM department d JOIN department_manager dm ON dm.departmentid = d.id JOIN employee emp ON dm.employeeid = emp.id ;";
+            try
+            {
+                var result = await connection.QueryAsync<DepartmentDto>(sql);
+                return new Response<List<DepartmentDto>>(result.ToList()); 
+            }
+            catch(Exception ex)
+            {
+                return new Response<List<DepartmentDto>>(System.Net.HttpStatusCode.InternalServerError, ex.Message);
+            }
+        }
+
+
+
+        public async Task<Response<List<DepartmentDto>>> GetDepartmentById(int id)
+        {
+            var connection = _context.CreateConnection();
+            string sql = "select d.id ,  d.name , dm.EmployeeId as MenegerId ,concat(emp.firstname , ' ' , emp.lastname) as ManagerFullName FROM department d JOIN department_manager dm ON dm.departmentid = d.id JOIN employee emp ON dm.employeeid = emp.id where d.id = @id;;";
+            try
+            {
+                var result = await connection.QueryAsync<DepartmentDto>(sql , new { id });
+                return new Response<List<DepartmentDto>>(result.ToList());
+            }
+            catch (Exception ex)
+            {
+                return new Response<List<DepartmentDto>>(System.Net.HttpStatusCode.InternalServerError, ex.Message);
+            }
+        }
+
+
+        public async Task<Response<Department>> AddDepartment(Department department)
+        {
+            var connection = _context.CreateConnection();
+            string sql = $"INSERT INTO Department (Id , Name) VALUES (@Id,@Name) ";
+            try
+            {
+                var result = await connection.ExecuteScalarAsync<Department>(sql, new { department.Id, department.Name});
+                department = result;
+                return new Response<Department>(department);
+            }
+            catch (Exception ex)
+            {
+                return new Response<Department>(System.Net.HttpStatusCode.InternalServerError, ex.Message);
+            }
+        }
+
+        public async Task<Response<Department>> UpdateDepartment(Department department)
+        {
+            using var connection = _context.CreateConnection();
+            string sql = $"UPDATE  Department SET Id = @Id, Name = @Name  WHERE id = @Id";
+            try
+            {
+                var response = await connection.ExecuteAsync(sql , new {department.Name , department.Id});
+                return new Response<Department>(department);
+            }
+            catch (Exception ex)
+            {
+                return new Response<Department>(System.Net.HttpStatusCode.InternalServerError, ex.Message);
+            }
+        }
+
+
+
+
+
+
+
+
+
+    }
+}
